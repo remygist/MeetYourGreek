ae4b051c56bb207c870350ea202427926b5e85b9
remygist <remy.gistelinck@student.ehb.be>
2023-12-18 17:32:10 +0100

    finished myths.html

---
+++ Myths.js
@@ -0,0 +1,27 @@
+export default
+class Myths{
+    constructor(id,title,description,image){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    get htmlContainerData(){
+        return`     
+        <div class="mythContainer">
+        <h1>${this.title}</h1>
+                <p>
+                    ${this.description}
+                </p></div><div class="imgContainer">
+                <img src="${this.image}" />
+            </div>
+        `
+    }
+
+    get htmlSidebarData(){
+        return`
+        <div class="mythItem" id="${this.id}"><h1>${this.title}</h1></div>
+        `
+    }
+}
---
+++ mythsScript.js
@@ -0,0 +1,67 @@
+"use strict";
+import Myths from "./Myths.js";
+
+const mythsArray = [];
+const pageContainer = document.querySelector(".mythContainer");
+const sidebarContainer = document.querySelector(".sidebar");
+
+async function getData() {
+    try {
+        const response = await fetch("http://localhost:3000/getMyths");
+        const data = await response.json();
+
+        console.log(data);
+        //storing data into classes
+        data.forEach((myth) => {
+            const mythData = new Myths(
+                myth._id,
+                myth.title,
+                myth.description,
+                myth.image
+            );
+            //pushing data to global array
+            mythsArray.push(mythData);
+        });
+        showMyths();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function showMyths() {
+    //clear previous content
+    sidebarContainer.innerHTML = "";
+    pageContainer.innerHTML = "";
+    mythsArray.forEach(function (myth) {
+        sidebarContainer.insertAdjacentHTML("beforeend", myth.htmlSidebarData);
+    });
+    pageContainer.insertAdjacentHTML(
+        "beforeend",
+        mythsArray[0].htmlContainerData
+    );
+    const selectedMyth = document.querySelectorAll(".mythItem");
+    showMythsData(selectedMyth);
+}
+
+function showMythsData(selectedMyth) {
+    console.log(mythsArray[0].htmlContainerData);
+
+    selectedMyth.forEach((data) => {
+        data.addEventListener("click", () => {
+            console.log("Clicked deity:", data);
+            const clickedMythId = data.id;
+            const clickedMyth = mythsArray.find(
+                (myth) => myth.id == clickedMythId
+            );
+            console.log(clickedMyth);
+            pageContainer.innerHTML = "";
+            pageContainer.insertAdjacentHTML(
+                "beforeend",
+                clickedMyth.htmlContainerData
+            );
+        });
+    });
+}
+
+getData();
+console.log(mythsArray);
