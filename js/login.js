ed84de28cb2133b7fa84b922d83dc8a989c050db
remygist <remy.gistelinck@student.ehb.be>
2023-12-20 16:24:32 +0100

    made it possible to send suggestions to db

--- js/login.js
+++ js/login.js
@@ -1,21 +1,26 @@
 "use strict"
 
-document.querySelector("#inputForm").addEventListener("submit", function (event) {
-    
 
-    let user = {}
+document.querySelector("#inputForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let user = {};
     user.username = document.querySelector("#username").value;
-    user.password = document.querySelector("#password").value
+    user.password = document.querySelector("#password").value;
 
     console.log(user);
 
-    //check for login
+    // Check for login
     getData("https://web-2-course-project-jayu.onrender.com/login", "POST", user).then(data => {
         console.log(data);
-    })
 
-
-})
+        // If login is successful, store the username in local storage and navigate to profile.html
+        if (data.status === "Authentication succesfull") {
+            localStorage.setItem('username', user.username);
+            window.location.href = 'profile.html';
+        }
+    });
+});
 
 async function getData(url, method, data) {
     let resp = await fetch(url, {
--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,8 @@
 "use strict"
-
-
+const usernameFromStorage = localStorage.getItem('username');
+console.log(usernameFromStorage);
 function showPage(){
-
+    
     const selectedPage = document.getElementById("suggestionSelector").value;
     console.log(selectedPage);
 
@@ -11,4 +11,78 @@ function showPage(){
     document.getElementById('inspirationsContent').style.display = "none";
 
     document.getElementById(selectedPage + "Content").style.display = "block"
+}
+
+//change password
+document.querySelector("#inputForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    let passwordChange = {}
+    passwordChange.username = usernameFromStorage
+    passwordChange.password = document.querySelector("#password").value
+    passwordChange.newPassword = document.querySelector("#newPassword").value
+
+    console.log(passwordChange);
+
+    getData("https://web-2-course-project-jayu.onrender.com/updateProfile", "PUT", passwordChange).then(data => {
+        console.log(data);
+    })
+
+
+})
+
+//suggest deity
+document.querySelector("#deitiesForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    let deitySuggest = {}
+    deitySuggest.name = document.querySelector("#nameDeity").value
+    deitySuggest.title = document.querySelector("#titleDeity").value
+    deitySuggest.description = document.querySelector("#descriptionDeity").value
+
+    console.log(deitySuggest);
+
+    getData("https://web-2-course-project-jayu.onrender.com/suggestDeity", "POST", deitySuggest).then(data => {
+        console.log(data);
+    })
+})
+
+//suggest myth
+document.querySelector("#mythsForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    let mythSuggest = {}
+    mythSuggest.title = document.querySelector("#titleMyth").value
+    mythSuggest.description = document.querySelector("#descriptionMyth").value
+
+    console.log(mythSuggest);
+
+    getData("https://web-2-course-project-jayu.onrender.com/suggestMyth", "POST", mythSuggest).then(data => {
+        console.log(data);
+    })
+})
+
+//suggest inspiration
+document.querySelector("#inspirationsForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    let inspirationSuggest = {}
+    inspirationSuggest.title = document.querySelector("#titleInspirations").value
+    inspirationSuggest.description = document.querySelector("#descriptionInspirations").value
+
+    console.log(inspirationSuggest);
+
+    getData("https://web-2-course-project-jayu.onrender.com/suggestDeity", "POST", inspirationSuggest).then(data => {
+        console.log(data);
+    })
+})
+
+async function getData(url, method, data) {
+    let resp = await fetch(url, {
+        method: method,
+        headers: { 'Content-Type': 'application/json' },
+        mode: "cors",
+        body: JSON.stringify(data)
+    });
+    return await resp.json();
 }
